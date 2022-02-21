ea602a1ac624d5d1d9e2f3298b6e98645a55e17b
C-Wesley <wcooke@augusta.edu>
2022-02-20 21:45:21 -0500

    Added vector stuff. Started refactoring the project

--- project/main.py
+++ project/main.py
@@ -10,6 +10,7 @@ main.py
 """
 import sys
 import time
+from cv2 import CAP_PROP_EXPOSURE
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -23,6 +24,9 @@ def fix_color(img):
     updated_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return updated_img
 
+
+cam = cv2.VideoCapture(0)
+
 PATH_TO_SAVED_MODEL = r'.\resources\model\saved_model'
 PATH_TO_LABELS = r'.\resources\annotations\label_map.pbtxt'
 
@@ -30,79 +34,99 @@ Detector = Model(PATH_TO_SAVED_MODEL, PATH_TO_LABELS)
 
 # Drone 1 has password of 12345678 
 
-cam = cv2.VideoCapture(1)
+
+
+print(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+
+
+
 
 # How many boxes do we want displaying? 
-num_boxes = 2
+num_boxes = 10
 
 # How confident does the model need to be to display any bouding box? 
 min_score_thresh = .70
 
-_, img = cam.read()
-#cv2.imshow('im1', img)
-#cv2.waitKey(0)
+# cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+# cam.set(cv2.CAP_PROP_EXPOSURE, -2)
 
-img = fix_color(img)
+for x in range(100):
+    _, img = cam.read()
 
-flag_centers = Detector.get_one_class_centers(img, 4, 2)
-flag_center = flag_centers[0]
-flag_center = (int(flag_center[0]), int(flag_center[1]))
-
-print(flag_centers)
-# get old positions
-pre_moving = Detector.IDS.get_current_positions()
-
-# Move the drones
-print('You have 10 seconds to move the drone')
-time.sleep(10)
-
-_, img2 = cam.read()
-img2 = fix_color(img2)
-
-img2 = np.array(img2)
-detections = Detector.detect(img2)
-Detector.update_ids(detections, num_boxes)
-
-# get new positions 
-post_moving = Detector.IDS.get_current_positions()
-
-print(pre_moving)
-print(post_moving)
-# compute the vectors
-
-
-
-flag1_vec = Detector.compute_2d_vector(flag_centers[0], post_moving[0]) # center - post 
-#flag2_vec = Detector.compute_2d_vector(flag_centers[1], post_moving[1])
-
-drone1dir = Detector.compute_2d_vector(post_moving[0], pre_moving[0]) # Post - Pre
-#drone2dir = Detector.compute_2d_vector(post_moving[1], pre_moving[1]) 
-
-drone1_angle = Detector.dotProduct_2d(drone1dir, flag1_vec) 
-#drone2_angle = Detector.dotProduct_2d(drone2dir, flag2_vec)
-
-img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
-
-img2 = cv2.circle(img2, flag_center, 3, (0,255,0), thickness=-1)
-img2 = cv2.circle(img2, pre_moving[0], 3, (0,0,255), thickness=-1)
-img2 = cv2.circle(img2, post_moving[0], 3, (0,0,255), thickness=-1)
-
-# Direction
-output = cv2.line(img2, post_moving[0],(post_moving[0][0] + drone1dir[0], post_moving[0][1] + drone1dir[1]), (255,0,255), thickness = 3)
-#output = cv2.line(output, post_moving[1],drone2dir, (255,0,255),thickness = 3)
-
-# Vector to flag
-output = cv2.line(output,  post_moving[0],(post_moving[0][0] + flag1_vec[0], post_moving[0][1] + flag1_vec[1]), (255,0,0),thickness = 3)
-#output = cv2.line(output,  post_moving[1],flag2_vec, (255,0,0),thickness = 3)
-
-cv2.imshow("output", output) 
+cv2.imshow('im1', img)
 cv2.waitKey(0)
+
+while True:
+    _, img = cam.read()
+
+    img = fix_color(img)
+
+    flag_centers = Detector.get_one_class_centers(img, 4, 2)
+
+    print(flag_centers)
+    if flag_centers:
+        flag_center = flag_centers[0]
+        flag_center = (int(flag_center[0]), int(flag_center[1]))
+    else:
+        continue
+
+
+    # get old positions
+    pre_moving = Detector.IDS.get_current_positions()
+
+    # Move the drones
+    print('You have 5 seconds to move the drone')
+    time.sleep(5)
+
+    _, img2 = cam.read()
+    img2 = fix_color(img2)
+
+    img2 = np.array(img2)
+    detections = Detector.detect(img2)
+    Detector.update_ids(detections, num_boxes)
+
+    # get new positions 
+    post_moving = Detector.IDS.get_current_positions()
+
+    print(pre_moving)
+    print(post_moving)
+    # compute the vectors
+
+
+
+    flag1_vec = Detector.compute_2d_vector(flag_centers[0], post_moving[0]) # center - post 
+    flag2_vec = Detector.compute_2d_vector(flag_centers[0], post_moving[1])
+
+    drone1dir = Detector.compute_2d_vector(post_moving[0], pre_moving[0]) # Post - Pre
+    drone2dir = Detector.compute_2d_vector(post_moving[1], pre_moving[1]) 
+
+    drone1_angle = Detector.dotProduct_2d(drone1dir, flag1_vec) 
+    drone2_angle = Detector.dotProduct_2d(drone2dir, flag1_vec)
+
+    img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
+
+    img2 = cv2.circle(img2, flag_center, 3, (0,255,0), thickness=-1)
+    img2 = cv2.circle(img2, pre_moving[0], 3, (0,0,255), thickness=-1)
+    img2 = cv2.circle(img2, post_moving[0], 3, (0,0,255), thickness=-1)
+
+    # Direction
+    output = cv2.line(img2, post_moving[0],(post_moving[0][0] + drone1dir[0], post_moving[0][1] + drone1dir[1]), (255,0,255), thickness = 3)
+    output = cv2.line(output, post_moving[1],(post_moving[1][0] + drone2dir[0], post_moving[1][1] + drone2dir[1]), (255,0,255),thickness = 3)
+
+    # Vector to flag
+    output = cv2.line(output,  post_moving[0],(post_moving[0][0] + flag1_vec[0], post_moving[0][1] + flag1_vec[1]), (255,0,0),thickness = 3)
+    output = cv2.line(output,  post_moving[1],(post_moving[1][0] + flag2_vec[0], post_moving[1][1] + flag2_vec[1]), (255,0,0),thickness = 3)
+
+    cv2.imshow("output", output) 
+    cv2.waitKey(0)
 
 
 
 # compute the angles :D 
 
-"""
+
 while True:
     ret, img = cam.read()
 
@@ -130,7 +154,7 @@ while True:
         cv2.imshow('Img', updated_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-"""
+
 """
 What's next?? 
 
